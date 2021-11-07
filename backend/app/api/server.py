e7c31cacbf1f82f85a39a8ed0a14452db38f1fab
iAnafem <sidviciouc@gmail.com>
2021-11-07 23:46:01 +0300

    Add postgres connection. Add env.template. Add base repository, etc

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from app.core import config, tasks
 
 
 def get_application():
     application = FastAPI(title="project_name", version="1.0.0")
+
     application.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -11,6 +13,10 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    application.add_event_handler("startup", tasks.create_start_app_handler(application))
+    application.add_event_handler("shutdown", tasks.create_stop_app_handler(application))
+
     return application
 
 
