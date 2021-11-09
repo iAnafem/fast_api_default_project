a663e5273ca8b6f044acb60c490cd01239397d5a
iAnafem <sidviciouc@gmail.com>
2021-11-09 23:17:45 +0300

    frontend server works

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,15 @@
-import React from "react";
-import ReactDom from "react-dom";
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
 import App from "./App";
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root'),
+)
