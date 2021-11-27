f4c548b65c141aa1be2ee1145053b9843b2369e4
iAnafem <sidviciouc@gmail.com>
2021-11-27 14:05:54 +0300

    fix db variable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,7 +12,7 @@ POSTGRES_USER = config("POSTGRES_USER", cast=str)
 POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
 POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
 POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
-POSTGRES_DB = config("POSTGRES_NAME", cast=str)
+POSTGRES_DB = config("POSTGRES_DB", cast=str)
 DATABASE_URL = config(
   "DATABASE_URL",
   cast=DatabaseURL,
